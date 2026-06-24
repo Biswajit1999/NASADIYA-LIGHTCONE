ae9c08f8a988ce855b74757b7c134e9c5ad18705
Biswajit Jana <49089568+Biswajit1999@users.noreply.github.com>
2026-06-25 00:47:03 +0100

    feat: configure full DESI GPU cloud

---
+++ src/full-cloud-config.js
@@ -0,0 +1,6 @@
+export const FULL_DESI_GPU_CLOUD = Object.freeze({
+  manifestPath: './data/processed/desi-dr1/full-cloud/full-cloud.json',
+  remoteBaseUrl: null,
+  expectedRows: 6_093_818,
+  minimumMemoryGiB: 16,
+});
